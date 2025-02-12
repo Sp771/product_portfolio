95b77c5a95b3c2328bffe3d2fdb5e57026f211b0
sp771 <sydney.m.pittman@gmail.com>
2025-02-12 15:00:42 -0500

    name change

--- components/intro.js
+++ components/intro.js
@@ -6,7 +6,7 @@ export default function Intro() {
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <div className="md:pr-8">
         <h1 className="text-6xl md:text-7xl font-bold leading-tight">
-          Hi, I'm Annie Apple,<br />Product Leader
+          Hi, I'm Sydney Pittman,<br />Product Leader
         </h1>
         <p className="text-lg block text-slate-500">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
           Velit officia consequat duis enim velit mollit. 
