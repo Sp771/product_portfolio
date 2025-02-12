cd38f6bf279446c6a152a40063560d0f58c5345e
sp771 <sydney.m.pittman@gmail.com>
2025-02-12 16:15:36 -0500

    profile pic

--- components/intro.js
+++ components/intro.js
@@ -5,9 +5,12 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <div className="md:pr-8">
-        <h1 className="text-6xl md:text-7xl font-bold leading-tight">
-          Hi, I'm Sydney Pittman,<br />Product Leader
-        </h1>
+      <h1 className="text-6xl md:text-7xl font-bold leading-tight">
+  Hi, I'm Sydney Pittman<br />
+  <span className="text-4xl md:text-5xl font-semibold text-gray-600">Product Leader</span>
+</h1>
+
+
         <p className="text-lg block text-slate-500">Results-driven Product Manager with expertise in Agile, product design, and mentorship. Focused on innovative, customer-centric solutions, team growth, and data-driven strategies to boost engagement and performance. Thrives in fast-paced, trend-driven environments.</p>
         <a href="/assets/resume/Resume.pdf"
           className="bg-accent-1 hover:bg-white hover:text-accent-1 border border-accent-1 text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mt-6 inline-block"
@@ -15,7 +18,7 @@ export default function Intro() {
           Download Resume
         </a>
       </div>
-      <Image title={"lisafeets"} src={"/assets/posts/authors/annie.svg"} height={295} width={293}
+      <Image title={"lisafeets"} src={"/assets/posts/authors/annie.svg"} height={700} width={700}
       />
     </section>
   )
