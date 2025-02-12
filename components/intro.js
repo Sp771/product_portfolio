4b0be3d1395eab0109646d28f7fcb1a2cd7e8a82
sp771 <sydney.m.pittman@gmail.com>
2025-02-12 15:34:03 -0500

    author pic and intro change

--- components/intro.js
+++ components/intro.js
@@ -8,9 +8,7 @@ export default function Intro() {
         <h1 className="text-6xl md:text-7xl font-bold leading-tight">
           Hi, I'm Sydney Pittman,<br />Product Leader
         </h1>
-        <p className="text-lg block text-slate-500">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. 
-          Velit officia consequat duis enim velit mollit. 
-          Exercitation veniam consequat sunt nostrud amet.</p>
+        <p className="text-lg block text-slate-500">Results-driven Product Manager with expertise in Agile, product design, and mentorship. Focused on innovative, customer-centric solutions, team growth, and data-driven strategies to boost engagement and performance. Thrives in fast-paced, trend-driven environments.</p>
         <a href="/assets/resume/Resume.pdf"
           className="bg-accent-1 hover:bg-white hover:text-accent-1 border border-accent-1 text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mt-6 inline-block"
         >
